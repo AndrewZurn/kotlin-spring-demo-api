ac8ca43aa840144783f2c8f7a506c372d3f202dc
Andrew Zurn <awzurn@gmail.com>
2018-05-10 18:32:29 -0700

    Adding logging filter

---
+++ src/main/kotlin/com/zalude/orders/utils/MyMiddleware.kt
@@ -0,0 +1,29 @@
+package com.zalude.orders.utils
+
+import mu.KLogging
+import org.springframework.stereotype.Component
+import java.io.IOException
+import javax.servlet.ServletException
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletRequest
+import org.springframework.web.filter.OncePerRequestFilter
+
+
+
+/**
+ * @author andrew.zurn@dexcom.com - 5/10/18.
+ */
+@Component
+class LoggingFilter : OncePerRequestFilter() {
+
+  @Throws(ServletException::class, IOException::class)
+  override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+    val start = System.currentTimeMillis()
+    filterChain.doFilter(request, response)
+    logger.info("${request.method} ${request.requestURI} took ${System.currentTimeMillis() - start}ms, resulted in status: ${response.status}")
+  }
+
+  companion object: KLogging()
+
+}
